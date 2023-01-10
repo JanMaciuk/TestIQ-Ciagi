ab6bb70fa22878344c817bd479ba1350af44aaee
JanMaciuk <38810823+JanMaciuk@users.noreply.github.com>
2023-01-10 11:32:59 +0100

    Add a gui sketch

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+import java.awt.*;
 import java.util.Random;
 import java.util.ArrayList;
 
@@ -15,7 +17,26 @@ public class Main {
         d = random.nextInt(1,5);
         r = random.nextInt(1,5);
         ArrayList truePattern = generojPattern();
+        ArrayList fakePattern = generojFake();
         printpattern(truePattern);
+        printpattern(fakePattern);
+
+    }
+
+    public static void createGUI() {
+        JFrame mainFrame = new JFrame("Test na ciągi");
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.getContentPane().setLayout(new GridLayout(1, 3));
+
+        //Panel 1
+        JPanel panel1 = new JPanel();
+        mainFrame.getContentPane().add(panel1);
+        panel1.setLayout(new GridLayout(3,4));
+
+        //Panel 2
+        JPanel panel2 = new JPanel();
+        mainFrame.getContentPane().add(panel2);
+        panel2.setLayout(new GridLayout(2,2));
 
     }
     protected static ArrayList<Integer> generojPattern() {
@@ -36,9 +57,14 @@ public class Main {
     protected static ArrayList<Integer> generojFake() {
         Random random = new Random();
         ArrayList<Integer> pattern = new ArrayList<>(a);
+        pattern.add(random.nextInt(1,10));
+            for (int i = 1; i <= 3; i++) {
+                int r = random.nextInt(1,100);
+                if (r > pattern.get(pattern.size()-1)) {pattern.add(r);}
+                else {
+                    i--;
+                }
 
-            for (int i = 1; i <= 4; i++) {
-                pattern.add(random.nextInt(1,100));
             }
             return pattern;
         }
